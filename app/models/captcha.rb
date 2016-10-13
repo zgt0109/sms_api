b7ac4b6b66b081713ba069bb20dd38e77a2b2d20
zgt <2506526350@qq.com>
2016-10-13 17:54:45 +0800

    一分钟发送的次数

--- app/models/captcha.rb
+++ app/models/captcha.rb
@@ -11,6 +11,21 @@
 #
 
 class Captcha < ApplicationRecord
+  validates :mobile, presence: true, format: {
+    with: /\A1(3[0-9]|4[57]|5[0-35-9]|7[01678]|8[0-9])\d{8}\z/,
+    message: '请输入正确的手机号码。'
+  }
+  validates :minute_size, inclusion: { in: [0], message: '短信验证码已经发送。' }
+
+  # 一分钟之内发送的次数
+  attr_accessor :minute_size
+
+  scope :send_after, -> (mobile, time){ where(mobile: mobile).where("send_at > ?", time) }
+
+  before_validation do
+    self.minute_size = Captcha.send_after(self.mobile, 1.minute.ago).size
+  end
+
   before_create do
     self.code = [*0..9].shuffle[0..5].join
   end
