93ead91b6876a84b177fffe1d9a9c84194577540
zgt <2506526350@qq.com>
2016-10-17 11:08:14 +0800

    修改select(:id, :code)

--- app/controllers/captchas_controller.rb
+++ app/controllers/captchas_controller.rb
@@ -3,7 +3,7 @@ class CaptchasController < ApplicationController
     captcha = current_sms_key.captchas.create(mobile: params[:mobile])
     rst = { data: '', code: '', msg: '' }
     if !captcha.send_at.nil?
-    	captcha.sms_key.increment!(:key_count)
+      captcha.sms_key.increment!(:key_count)
       rst[:code] = :created
       rst[:msg] = '短信验证码发送成功'
     else
@@ -14,26 +14,26 @@ class CaptchasController < ApplicationController
     render json: rst
   end
 
-	def show
-		mobile = params[:mobile]
-		code = params[:code]
-		ncode = Captcha.select(:code).where("mobile = ?", mobile).limit(1).order('id desc')
-		result = {msg: "", code: "" ,data: ""}
-		if  code.empty? || ncode.empty?
-			result["msg"] = "验证码不正确"
-			result["code"] = "404"
-			render json: result
-			return
-		end
-		if code != ncode[0]["code"]
-			result["msg"] = "验证码不正确"
-			result["code"] = "401"
-			render json: result
-			return
-		end
-		result["msg"] ="验证码成功"
-		result["code"] = :ok
-		result["data"] = ncode
-		render json: result
-	end
+  def show
+    mobile = params[:mobile]
+    code = params[:code]
+    ncode = Captcha.select(:id, :code).where("mobile = ?", mobile).limit(1).order('id desc')
+    result = {msg: "", code: "" ,data: ""}
+    if  code.empty? || ncode.empty?
+      result["msg"] = "验证码不正确"
+      result["code"] = "404"
+      render json: result
+      return
+    end
+    if code != ncode[0]["code"]
+      result["msg"] = "验证码不正确"
+      result["code"] = "401"
+      render json: result
+      return
+    end
+    result["msg"] ="验证码成功"
+    result["code"] = :ok
+    result["data"] = ncode
+    render json: result
+  end
 end
