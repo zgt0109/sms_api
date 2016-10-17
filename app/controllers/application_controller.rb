ebce558fa9c1da0b5564c81bec2f46c5b9cbafb4
zgt <2506526350@qq.com>
2016-10-17 10:39:26 +0800

    通过key_count排序，取发送条数少的KEY来发送短信

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-	def current_sms_key
-		SmsKey.first
-	end
+  def current_sms_key
+    SmsKey.key_val.first
+  end
 end
--- app/models/sms_key.rb
+++ app/models/sms_key.rb
@@ -11,12 +11,6 @@
 #
 
 class SmsKey < ApplicationRecord
-	has_many :captchas
-
-	#key_val:发送最少的平台的key值
-	attr_accessor :key_val
-	scope :send_before, -> {select(:key).limit(1).order("key_count ASC")}
-	before_validation do
-		self.key_val = Captcha.send_before.size
-	end 
+  has_many :captchas
+  scope :key_val, -> { order("key_count ASC") }
 end
